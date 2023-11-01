fb3a7919e086421443462eecc684f0d68412a843
17LingShan <huangruofeng9@gmail.com>
2023-11-02 00:43:39 +0800

    :green_heart: fix ci build

--- src/components/recommendVideo/RecommendVideo.tsx
+++ src/components/recommendVideo/RecommendVideo.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from "react"
 import { observer } from "mobx-react"
-import "./style/recommendVideo.scss"
+import "./style/RecommendVideo.scss"
 
 interface Props {
   isCurrent: boolean
