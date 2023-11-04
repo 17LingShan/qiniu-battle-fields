b29c5e2c067fd5f1b13a4950341d6e3a482d496d
17LingShan <huangruofeng9@gmail.com>
2023-11-04 10:24:21 +0800

    ::art:: style: use input[range] in process bar

--- src/components/recommendVideo/RecommendVideo.tsx
+++ src/components/recommendVideo/RecommendVideo.tsx
@@ -1,4 +1,4 @@
-import { ChangeEventHandler, MouseEventHandler, useEffect, useRef, useState } from "react"
+import { useEffect, useRef, ChangeEventHandler, MouseEventHandler } from "react"
 import { toJS } from "mobx"
 import { observer } from "mobx-react"
 import { FaPause, FaPlay } from "react-icons/fa6"
@@ -16,11 +16,7 @@ interface Props {
 function RecommendVideo({ index, isCurrent, src }: Props) {
   const videoRef = useRef<HTMLVideoElement>(null)
   const controlsRef = useRef<HTMLDivElement>(null)
-  const videoProcessRef = useRef<HTMLDivElement>(null)
-  const videoProcessWrapRef = useRef<HTMLDivElement>(null)
   const volumeControlsBarRef = useRef<HTMLDivElement>(null)
-
-  const [keydown, setKeydown] = useState(false)
 
   const handleControlKeydown = (event: KeyboardEvent) => {
     console.log(event.key)
@@ -67,14 +63,6 @@ function RecommendVideo({ index, isCurrent, src }: Props) {
     videoRef.current!.muted = !videoRef.current?.muted
   }
 
-  const handleEnterFigure = () => {
-    controlsRef.current?.classList.add("video-controls-wrap-hover")
-  }
-
-  const handleLeaveFigure = () => {
-    controlsRef.current?.classList.remove("video-controls-wrap-hover")
-  }
-
   const handleEnterVolumeBar = () => {
     RecommendVideoStore.setVolumeBarFocus(true)
   }
@@ -106,34 +94,12 @@ function RecommendVideo({ index, isCurrent, src }: Props) {
 
   const handleTimeUpdate = () => {
     RecommendVideoStore.setCurrentTime(videoRef.current!.currentTime)
-    videoProcessRef.current!.style.width = `${(videoRef.current!.currentTime / videoRef.current!.duration) * 100}%`
   }
 
-  const handleUpdateProcess = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    const totalWidth = videoProcessWrapRef.current!.getBoundingClientRect().width
-    const barLeft = videoProcessRef.current!.getBoundingClientRect().left
-    const mouseX = event.pageX
-    const offsetX = mouseX - barLeft
-    const percentage = offsetX / totalWidth
-    videoProcessRef.current!.style.width = `${percentage * 100}%`
-    videoRef.current!.currentTime = percentage * videoRef.current!.duration
-  }
-
-  const handleKeyUpProcess = () => {
-    console.log("key up")
-    setKeydown(false)
-  }
-
-  const handleKeydownProcess = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    console.log("key down")
-    setKeydown(true)
-    handleUpdateProcess(event)
-  }
-
-  const handleKeyMovePRocess = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    if (keydown && isCurrent) {
-      handleUpdateProcess(event)
-    }
+  const handleProcessChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    const processValue = +event.currentTarget.value
+    videoRef.current!.currentTime = processValue
+    handleTimeUpdate()
   }
 
   const handleVolumeChange: ChangeEventHandler<HTMLInputElement> = (event) => {
@@ -179,7 +145,7 @@ function RecommendVideo({ index, isCurrent, src }: Props) {
 
   return (
     <>
-      <figure className='figure-video' onMouseEnter={handleEnterFigure} onMouseLeave={handleLeaveFigure}>
+      <figure className='figure-video'>
         <video
           className='video-instance'
           id='video'
@@ -198,14 +164,15 @@ function RecommendVideo({ index, isCurrent, src }: Props) {
         </span>
         <div className='video-controls-wrap' ref={controlsRef}>
           <div className='video-controls-container'>
-            <div
-              className='video-process-outline'
-              ref={videoProcessWrapRef}
-              onMouseUp={handleKeyUpProcess}
-              onMouseDown={handleKeydownProcess}
-              onMouseMove={handleKeyMovePRocess}
-            >
-              <div className='video-process-inline' ref={videoProcessRef}></div>
+            <div className='video-process-outline'>
+              <input
+                type='range'
+                step={0.5}
+                min={0}
+                max={videoRef.current?.duration || 0}
+                value={toJS(RecommendVideoStore.currentTime)}
+                onChange={handleProcessChange}
+              />
             </div>
             <div className='video-controls-left-tool'>
               <div className='flex-hor-ver-center' onClick={handleSpaceKeydown}>
